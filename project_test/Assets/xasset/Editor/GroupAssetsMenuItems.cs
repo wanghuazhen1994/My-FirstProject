b69d5a7184da62b8a7daa24d21a2498e658d2445
CHINAMI-63BJG2K\Administrator <wanghuazhen@fotoable.com>
2025-03-13 15:42:02 +0800

    提交修改

--- project_test/Assets/xasset/Editor/GroupAssetsMenuItems.cs
+++ project_test/Assets/xasset/Editor/GroupAssetsMenuItems.cs
@@ -4,38 +4,18 @@ namespace xasset.editor
 {
 	public static class GroupAssetsMenuItems
 	{
-		[MenuItem("Assets/Group To/Arts/Startup")]
-		private static void GroupToArtsStartup()
-		{
-			Settings.MakeSelectionAssetsGroupTo("Arts", "Startup");
-			Debug.Log("Group to Startup with build Arts.");
-		}
-
-		[MenuItem("Assets/Group To/Arts/Download")]
-		private static void GroupToArtsDownload()
-		{
-			Settings.MakeSelectionAssetsGroupTo("Arts", "Download");
-			Debug.Log("Group to Download with build Arts.");
-		}
-
-		[MenuItem("Assets/Group To/Arts/LoadAsset")]
-		private static void GroupToArtsLoadAsset()
-		{
-			Settings.MakeSelectionAssetsGroupTo("Arts", "LoadAsset");
-			Debug.Log("Group to LoadAsset with build Arts.");
-		}
-
-		[MenuItem("Assets/Group To/Arts/LoadAdditiveScene")]
-		private static void GroupToArtsLoadAdditiveScene()
-		{
-			Settings.MakeSelectionAssetsGroupTo("Arts", "LoadAdditiveScene");
-			Debug.Log("Group to LoadAdditiveScene with build Arts.");
-		}
 		[MenuItem("Assets/Group To/MyBuilder/Prefabs")]
 		private static void GroupToMyBuilderPrefabs()
 		{
 			Settings.MakeSelectionAssetsGroupTo("MyBuilder", "Prefabs");
 			Debug.Log("Group to Prefabs with build MyBuilder.");
+		}
+
+		[MenuItem("Assets/Group To/MyBuilder/EnterScenes")]
+		private static void GroupToMyBuilderEnterScenes()
+		{
+			Settings.MakeSelectionAssetsGroupTo("MyBuilder", "EnterScenes");
+			Debug.Log("Group to EnterScenes with build MyBuilder.");
 		}
 	}
 }
